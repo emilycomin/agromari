a39bfc7ef77f42ca2f92c79c9b01ab5349dc5813
Emily Comin <emilycominn@gmail.com>
2023-12-12 14:30:54 -0300

    criando o ItemCount para a entrega da aula 5, componentes II

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,14 +3,35 @@ import { useState } from "react";
 export default function ItemCount({ stock, initial, onAdd }) {
   const [count, setCount] = useState(1);
 
-  function addClick() {
+  function addProduto() {
     setCount(count + 1);
     console.log({ count });
   }
+  function tirarProduto() {
+    setCount(count - 1);
+    console.log({ count });
+  }
+
   return (
     <>
-      <button onClick={addClick}>Adicionar ao Carrinho</button>
-      <div>{count}</div>
+      <div className="comprarContent">
+        <div className="quantidade">
+          <button className="adicionar" onClick={addProduto}>
+            +
+          </button>
+          <input
+            type="text"
+            className="quantidadeProduto"
+            value={initial}
+            max={stock}
+            id=""
+          />
+          <button className="remover" onClick={tirarProduto}>
+            -
+          </button>
+        </div>
+        <button className="addCarrinho">Adicionar no carrinho</button>
+      </div>
     </>
   );
 }
